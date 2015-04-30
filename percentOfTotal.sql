60a4f365508ddc8ffd42ebc3629c03db39a045f5
eselyavka <eselyavka@yotateam.com>
2015-04-30 20:44:58 +0300

    add pipe example

--- percentOfTotal.sql
+++ percentOfTotal.sql
@@ -5,3 +5,53 @@ SELECT field
 FROM table_name
 GROUP BY field
 ORDER BY count(*) DESC;
+
+--Example pipe with 10 major goods and other
+WITH RECURSIVE allGoods (
+ goods_name
+ ,PERCENT
+ )
+AS (
+ SELECT NAME
+  ,100 * (count(*) / sum(count(*)) OVER ())
+ FROM goodsTable
+ GROUP BY NAME
+ ORDER BY count(*) DESC
+ )
+ ,tenMainGoods (
+ goods_name
+ ,PERCENT
+ )
+AS (
+ SELECT *
+ FROM allGoods limit 10
+ )
+ ,othergoods (
+ goods_name
+ ,PERCENT
+ )
+AS (
+ SELECT *
+ FROM allGoods
+ 
+ EXCEPT
+ 
+ SELECT *
+ FROM tenMainGoods
+ )
+ ,finalPipe (
+ goods_name
+ ,PERCENT
+ )
+AS (
+ SELECT *
+ FROM tenMainGoods
+ 
+ UNION ALL
+ 
+ SELECT 'other'
+  ,sum(PERCENT)
+ FROM othergoods
+ )
+SELECT *
+FROM finalPipe
